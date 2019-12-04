318228b31ca6a0d7e9e2c28980ba078771375368
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-04 16:26:45 +0530

    Changing text color when clicked

---
+++ assets/js/todos.js
@@ -0,0 +1,13 @@
+$("li").click(function(){
+    if($(this).css("color") === "rgb(128, 128, 128)"){
+        $(this).css({
+            color:"black",
+            textDecoration: "none"
+        });
+    } else{
+        $(this).css({
+            color:"gray",
+            textDecoration: "line-through"
+        });
+    }
+})
